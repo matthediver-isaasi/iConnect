1977ec149d7c35e7be2e6c262f4017d52c01aa10
isaasi <mat.the.diver@googlemail.com>
2025-11-25 16:26:26 +0100

    Removed base44

--- src/api/functions.js
+++ src/api/functions.js
@@ -1,97 +1,58 @@
-import { base44 } from './base44Client';
+// src/api/functions.js
 
+// TEMPORARY MIGRATION FILE
+// Base44 functions have been disabled.
+// Any code that tries to call these should be migrated to Supabase or Vercel functions.
 
-export const zohoOAuthCallback = base44.functions.zohoOAuthCallback;
-
-export const syncMemberFromCRM = base44.functions.syncMemberFromCRM;
-
-export const syncEventsFromBackstage = base44.functions.syncEventsFromBackstage;
-
-export const getZohoAuthUrl = base44.functions.getZohoAuthUrl;
-
-export const validateMember = base44.functions.validateMember;
-
-export const refreshMemberBalance = base44.functions.refreshMemberBalance;
-
-export const syncBackstageEvents = base44.functions.syncBackstageEvents;
-
-export const testFunction = base44.functions.testFunction;
-
-export const validateColleague = base44.functions.validateColleague;
-
-export const createBooking = base44.functions.createBooking;
-
-export const processProgramTicketPurchase = base44.functions.processProgramTicketPurchase;
-
-export const sendMagicLink = base44.functions.sendMagicLink;
-
-export const verifyMagicLink = base44.functions.verifyMagicLink;
-
-export const syncOrganizationContacts = base44.functions.syncOrganizationContacts;
-
-export const zohoContactWebhook = base44.functions.zohoContactWebhook;
-
-export const updateExpiredVouchers = base44.functions.updateExpiredVouchers;
-
-export const createStripePaymentIntent = base44.functions.createStripePaymentIntent;
-
-export const getStripePublishableKey = base44.functions.getStripePublishableKey;
-
-export const getXeroAuthUrl = base44.functions.getXeroAuthUrl;
-
-export const xeroOAuthCallback = base44.functions.xeroOAuthCallback;
-
-export const refreshXeroToken = base44.functions.refreshXeroToken;
-
-export const createXeroInvoice = base44.functions.createXeroInvoice;
-
-export const validateUser = base44.functions.validateUser;
-
-export const applyDiscountCode = base44.functions.applyDiscountCode;
-
-export const debugBackstageEvent = base44.functions.debugBackstageEvent;
-
-export const processBackstageCancellation = base44.functions.processBackstageCancellation;
-
-export const cancelBackstageOrder = base44.functions.cancelBackstageOrder;
-
-export const cancelTicketViaFlow = base44.functions.cancelTicketViaFlow;
-
-export const updateEventImage = base44.functions.updateEventImage;
-
-export const updateProgramDetails = base44.functions.updateProgramDetails;
-
-export const testEmailAddressInBackstage = base44.functions.testEmailAddressInBackstage;
-
-export const clearBookings = base44.functions.clearBookings;
-
-export const clearProgramTicketTransactions = base44.functions.clearProgramTicketTransactions;
-
-export const cancelProgramTicketTransaction = base44.functions.cancelProgramTicketTransaction;
-
-export const reinstateProgramTicketTransaction = base44.functions.reinstateProgramTicketTransaction;
-
-export const checkMemberStatusByEmail = base44.functions.checkMemberStatusByEmail;
-
-export const createJobPostingMember = base44.functions.createJobPostingMember;
-
-export const createJobPostingNonMember = base44.functions.createJobPostingNonMember;
-
-export const handleJobPostingPaymentWebhook = base44.functions.handleJobPostingPaymentWebhook;
-
-export const createJobPostingPaymentIntent = base44.functions.createJobPostingPaymentIntent;
-
-export const renameResourceSubcategory = base44.functions.renameResourceSubcategory;
-
-export const generateMemberHandles = base44.functions.generateMemberHandles;
-
-export const extractAndUploadFont = base44.functions.extractAndUploadFont;
-
-export const enableLoginForAllMembers = base44.functions.enableLoginForAllMembers;
-
-export const sendTeamMemberInvite = base44.functions.sendTeamMemberInvite;
-
-export const exportAllData = base44.functions.exportAllData;
-
-export const generateSitemap = base44.functions.generateSitemap;
-
+const notImplemented = (name) => () => {
+    throw new Error(`Base44 function "${name}" is not implemented. Migrate this feature to Supabase/Vercel.`);
+  };
+  
+  export const zohoOAuthCallback = notImplemented("zohoOAuthCallback");
+  export const syncMemberFromCRM = notImplemented("syncMemberFromCRM");
+  export const syncEventsFromBackstage = notImplemented("syncEventsFromBackstage");
+  export const getZohoAuthUrl = notImplemented("getZohoAuthUrl");
+  export const validateMember = notImplemented("validateMember");
+  export const refreshMemberBalance = notImplemented("refreshMemberBalance");
+  export const syncBackstageEvents = notImplemented("syncBackstageEvents");
+  export const testFunction = notImplemented("testFunction");
+  export const validateColleague = notImplemented("validateColleague");
+  export const createBooking = notImplemented("createBooking");
+  export const processProgramTicketPurchase = notImplemented("processProgramTicketPurchase");
+  export const sendMagicLink = notImplemented("sendMagicLink");
+  export const verifyMagicLink = notImplemented("verifyMagicLink");
+  export const syncOrganizationContacts = notImplemented("syncOrganizationContacts");
+  export const zohoContactWebhook = notImplemented("zohoContactWebhook");
+  export const updateExpiredVouchers = notImplemented("updateExpiredVouchers");
+  export const createStripePaymentIntent = notImplemented("createStripePaymentIntent");
+  export const getStripePublishableKey = notImplemented("getStripePublishableKey");
+  export const getXeroAuthUrl = notImplemented("getXeroAuthUrl");
+  export const xeroOAuthCallback = notImplemented("xeroOAuthCallback");
+  export const refreshXeroToken = notImplemented("refreshXeroToken");
+  export const createXeroInvoice = notImplemented("createXeroInvoice");
+  export const validateUser = notImplemented("validateUser");
+  export const applyDiscountCode = notImplemented("applyDiscountCode");
+  export const debugBackstageEvent = notImplemented("debugBackstageEvent");
+  export const processBackstageCancellation = notImplemented("processBackstageCancellation");
+  export const cancelBackstageOrder = notImplemented("cancelBackstageOrder");
+  export const cancelTicketViaFlow = notImplemented("cancelTicketViaFlow");
+  export const updateEventImage = notImplemented("updateEventImage");
+  export const updateProgramDetails = notImplemented("updateProgramDetails");
+  export const testEmailAddressInBackstage = notImplemented("testEmailAddressInBackstage");
+  export const clearBookings = notImplemented("clearBookings");
+  export const clearProgramTicketTransactions = notImplemented("clearProgramTicketTransactions");
+  export const cancelProgramTicketTransaction = notImplemented("cancelProgramTicketTransaction");
+  export const reinstateProgramTicketTransaction = notImplemented("reinstateProgramTicketTransaction");
+  export const checkMemberStatusByEmail = notImplemented("checkMemberStatusByEmail");
+  export const createJobPostingMember = notImplemented("createJobPostingMember");
+  export const createJobPostingNonMember = notImplemented("createJobPostingNonMember");
+  export const handleJobPostingPaymentWebhook = notImplemented("handleJobPostingPaymentWebhook");
+  export const createJobPostingPaymentIntent = notImplemented("createJobPostingPaymentIntent");
+  export const renameResourceSubcategory = notImplemented("renameResourceSubcategory");
+  export const generateMemberHandles = notImplemented("generateMemberHandles");
+  export const extractAndUploadFont = notImplemented("extractAndUploadFont");
+  export const enableLoginForAllMembers = notImplemented("enableLoginForAllMembers");
+  export const sendTeamMemberInvite = notImplemented("sendTeamMemberInvite");
+  export const exportAllData = notImplemented("exportAllData");
+  export const generateSitemap = notImplemented("generateSitemap");
+  
